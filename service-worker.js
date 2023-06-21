7f83b81967e7b65f525e29fb5cdd641247134f67
davifelipef <feliperedfield@gmail.com>
2023-06-21 12:14:32 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,41 @@
-const CACHE_NAME = 'june-party-calculator-v1';
+const CACHE_NAME = 'june-party-calculator-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/src/barbecue.png',
-  '/src/corn.png',
-  '/src/green-soup.png',
-  '/src/hot-dog.png',
-  '/src/hot-drink.png',
-  '/src/ice-cream.png',
-  '/src/juice.png',
-  '/src/pizza.png',
-  '/src/popcorn.png',
-  '/src/sausage.png',
-  '/src/small-soda.png',
-  '/src/water-bottle.png',
+  '/june_party_calculator/',
+  '/june_party_calculator/index.html',
+  '/june_party_calculator/style.css',
+  '/june_party_calculator/script.js',
+  '/june_party_calculator/src/barbecue.png',
+  '/june_party_calculator/src/corn.png',
+  '/june_party_calculator/src/green-soup.png',
+  '/june_party_calculator/src/hot-dog.png',
+  '/june_party_calculator/src/hot-drink.png',
+  '/june_party_calculator/src/ice-cream.png',
+  '/june_party_calculator/src/juice.png',
+  '/june_party_calculator/src/pizza.png',
+  '/june_party_calculator/src/popcorn.png',
+  '/june_party_calculator/src/sausage.png',
+  '/june_party_calculator/src/small-soda.png',
+  '/june_party_calculator/src/water-bottle.png',
 ];
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          return cache.addAll(urlsToCache);
+        })
+    );
+});  
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('activate', function(event) {
+event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+    return Promise.all(
+        cacheNames.filter(function(cacheName) {
+        }).map(function(cacheName) {
+        return caches.delete(cacheName);
+        })
+        );
+    })
+    );
 });
